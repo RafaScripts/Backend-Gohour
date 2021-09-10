97dcf4d83970ffcd608e7a887304a910a62872e4
Rafael Santos Freire <contato@rafascripts.site>
2021-09-10 02:18:44 -0300

    Update
    manipulação de alteração de usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -21,9 +21,26 @@ class UserController {
     }
 
 
-    async update(req, res){
-        return res.json({ ok: true });
+    async update(req, res) {
+        //buscar email e senha que serão alterados
+        const { email, oldPassword } = req.body;
+
+        //buscar usuario por ID no banco de dados
+        const user = await User.findByPk(req.userId);
+
+        //verifica se o email da requisição é o mesmo que consta no banco de dados
+        if (email !== user.email ){
+            //faz a busca pelo email
+            const userExists = await User.findOne({ where: { email: req.body.email } });
+
+            //se a busca retornar verdadeiro indicar que usuario ja foi cadastrado
+            if(userExists){
+                return res.status(400).json({ error: "Email already exist." });
+            }
+        }
+
     }
+    
 }
 
 export default new UserController();
