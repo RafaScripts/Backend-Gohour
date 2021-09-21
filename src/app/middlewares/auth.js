47cf803bd47af2961f9ecc2f298361e9cb11e019
Rafael Santos Freire <contato@rafascripts.site>
2021-09-21 01:28:36 -0300

    update
    Validação schema completa

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -21,6 +21,6 @@ export default async (req, res, next) => {
 
     return next();
   }catch (err) {
-    return res.status(401).json({ error: "token invalido!" });
+    return res.status(401).json({ error: "token invalid!" });
   }
 };
